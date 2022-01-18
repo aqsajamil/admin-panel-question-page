125aeb302a3040d899c2bbc1658f2e591293337d
aqsa Jamil <38866826+aqsajamil@users.noreply.github.com>
2022-01-19 00:06:14 +0500

    side nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     console.log("JQuery is ready!!!")
+    $(".iconBar a").click(function(){
+        $(this).siblings().removeClass("active");
+        $(this).addClass("active");
+    })
 })
